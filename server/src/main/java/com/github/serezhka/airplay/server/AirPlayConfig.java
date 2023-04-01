4e05f260fbe2ed3b9ae8a1e6c01d330cd06f6f5e
G Remus <gabriel.grld@gmail.com>
2023-04-01 03:40:06 -0300

    Set default values for AirPlayConfig
    Ideally, this values would be parsed from some configuration file, like
    the original java-airplay

--- server/src/main/java/com/github/serezhka/airplay/server/AirPlayConfig.java
+++ server/src/main/java/com/github/serezhka/airplay/server/AirPlayConfig.java
@@ -4,8 +4,8 @@ import lombok.Data;
 
 @Data
 public class AirPlayConfig {
-    private String serverName;
-    private int width;
-    private int height;
-    private int fps;
+    private String serverName = "DroidPlay";
+    private int width = 1080;
+    private int height = 1920;
+    private int fps = 24;
 }
